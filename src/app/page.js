fddfcb762b11447f8c9005f9c7e16bd0c1dfcd35
DESKTOP-8QGA0E7\user <nurnatasha413@yahoo.com>
2025-03-23 21:21:39 +0800

    DEV: Nav bar

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import AboutSection from "./components/AboutSection";
 
 // pages/index.js
 export default function Home() {
@@ -18,6 +19,7 @@ export default function Home() {
       <main className="flex min-h-screen container">
         <div className="container mt-24 mx-auto px-12 py-4">
           <HeroSection/>
+          <AboutSection/>
           </div>
       </main>
     </div>
