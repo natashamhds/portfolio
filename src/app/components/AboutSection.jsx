02a696669a74f5cb561340670665f5d1cafc434b
DESKTOP-8QGA0E7\user <nurnatasha413@yahoo.com>
2025-03-27 11:34:04 +0800

    DEV: Replaced absolute image path

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -17,7 +17,7 @@ const AboutSection = () => {
     <section className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image
-          src="/images/about-image.png"
+          src="./images/about-image.png"
           alt="screen-image"
           width={500}
           height={500}
